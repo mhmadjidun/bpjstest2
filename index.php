c23961790e61d91749e49b539af4cf5bcd251722
mhmadjidun <mhmadjidun@gmail.com>
2024-07-30 20:35:05 +0700

    Test

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include 'asset.php'
 ?>
 
 <div class="container mt-5">
-    <h2>Cek Saldo</h2>
+    <h2>Cek Saldo Testing</h2>
     <form method="get" action="cek.php">
         <div class="mb-3">
             <label for="norek" class="form-label">Nomor Rekening</label>
