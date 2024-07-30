b8bf9ebc113c6b8bb89ec3da99d1e86ac089da98
mhmadjidun <mhmadjidun@gmail.com>
2024-07-30 20:38:53 +0700

    Test2

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include 'asset.php'
     <h2>Cek Saldo Testing</h2>
     <form method="get" action="cek.php">
         <div class="mb-3">
-            <label for="norek" class="form-label">Nomor Rekening</label>
+            <label for="norek" class="form-label">Nomor Rekening Zidan Testing Woy</label>
             <input type="text" class="form-control" id="norek" name="norek" required>
         </div>
         <button type="submit" class="btn btn-primary">Cek Saldo</button>
